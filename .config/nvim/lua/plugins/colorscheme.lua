b15f67a10304f39b07af4fa77a3d38d84a2b91fa
Alexandro Leon <alexandrollj@gmail.com>
2025-05-02 21:01:27 -0600

    feat(nvim): update NvimTree windows to be transaparent when NC

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,10 @@ return {
 	config = function()
 		vim.cmd([[colorscheme tokyonight]])
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+		vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=NONE]])
+		vim.cmd([[hi NvimTreeNormalNC guibg=NONE ctermbg=NONE]])
 	end,
 }
