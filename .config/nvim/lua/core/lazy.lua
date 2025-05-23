52d2465088d2b297750421ca062e3ba460e25323
= <alexandrollj@gmail.com>
2025-05-22 18:17:06 -0600

    style(nvim): Add rounded borders to Lazy plugin manager

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -29,4 +29,5 @@ require("lazy").setup({
     install = { colorscheme = { "habamax" } },
     -- automatically check for plugin updates
     checker = { enabled = false },
+    ui = { border = "rounded" },
 })
--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -1,14 +1,14 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
 
-	config = function()
-		local lualine = require("lualine")
-		lualine.setup({
-			options = {
-				icons_enabled = true,
-				theme = "dracula",
-			},
-		})
-	end,
+    config = function()
+        local lualine = require("lualine")
+        lualine.setup({
+            options = {
+                icons_enabled = true,
+                theme = "material",
+            },
+        })
+    end,
 }
